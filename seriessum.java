34ce8801ad41ff916958ab89ff602e62cf23ed65
VISVANATH123 <viswa3643@gmail.com>
2025-01-28 10:18:54 +0530

    version1

---
+++ seriessum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class seriessum {
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        int n=scan.nextInt();
+        for(int i=1;i<=n;i++){
+            System.out.print(a+" ");
+            if(i!=n){
+                System.out.print("+ ");
+            }
+            a=a*10+3;
+           
+        }
+
+    }
+    }
+
