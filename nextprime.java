079ba254ed666616cf5bcae986304ed6943a9225
VISVANATH123 <viswa3643@gmail.com>
2025-01-28 10:15:29 +0530

    Add nextprime.java

---
+++ nextprime.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+public class nextprime
+{
+	public static void main(String[] args) {
+		int count=0;
+		Scanner scan = new Scanner (System.in);
+		int n=scan.nextInt();
+		while(count<5){
+		    n++;
+		    if(is(n)){
+		        System.out.print(n+" ");
+		        count++;
+		    }
+		    
+		}
+		scan.close();
+	}
+	public static boolean is(int n){
+	    if (n <=1) return false;
+	    for(int i=2;i<=Math.sqrt(n);i++){
+	        if (n %i == 0){
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+}
